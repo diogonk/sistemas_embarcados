dc29e4a2eac9f45f4473ac058d2f8c1e4237698b
Diogo <diogonk@yahoo.com.br>
2019-04-27 12:05:47 -0300

    lab 1 finished

--- es_user/src/func_hz.s
+++ es_user/src/func_hz.s
@@ -14,42 +14,30 @@ count_pulse_hz
   ; Funcao read_freq_hz
   ; Parametro de entrada: N�o tem
   ; Parametro de saida: R0 - Contagem de pulsos alternados
-  PUSH {R4,R5,R6,R7}
+  PUSH {R1, R2, R3, R4, R5}
   MOV R0, #0        ;toogle counter
-  MOV R1, #0        ;last state
   MOV R2, #0        ;read gpio
   MOV R3, #0        ;time acquire
   LDR R4, =GPIO_PORTK_AHB_DATA_BITS_R				;Carrega o valor do offset do data register
   ADD R4, R4, #0x04	    ;Soma ao offset o endereço do bit0 para leitura amigavel [BIT0]
-  MOV R5, #0        ;time sampling
-  MOV R6, #0        ;for khz purpose
-  MOV R7, #0x3600   ; 240E6 
-  MOVT R7, #0x016E   ; são 240M ciclos de máquina, o que representa 1 segundo
+  //MOV  R5, #0xE1C0    ; Em 8 ciclos de máquina, 15.000.000 de leitura podem ser realizadas 
+  //MOVT R5, #0x00E4    ; considerando um PLL de 120MHz
+  MOV  R5, #0xC6C0    ; Em 8 ciclos de máquina, 3.000.000 de leitura podem ser realizadas 
+  MOVT R5, #0x002D    ; considerando um PLL de 24MHz
+  LDR R1, [R4]      ;Last State
+
 NEWSAMPLING
   LDR R2, [R4]      ;Leitura GPIO
   CMP R2, R1
   ITT NE
   ADDNE R0, #0x01   ;estado anterior diferente do atual, conta um pulso
   MOVNE R1, R2      ;salva estado anterior
-  CMP R3, R7        ;verifica se já terminou a aquisição
-  BGE EXITFUNC
+  SUBS R5, #1     
+  BPL NEWSAMPLING   ;verifica se já terminou a aquisição
   
-  NOP               ;Just to have even number of cycles
-  ADD R5, R5, #10
-WAITSAMPLING
-  NOP
-  CMP R5, #240     
-  ITTT GE
-  ADDGE R5, R5, #9
-  ADDGE R3, R3, R5
-  SUBGE R5, R5, #240
-  BGE NEWSAMPLING
 
-  ADD R5, R5, #10
-  B WAITSAMPLING
-
-EXITFUNC
-  POP {R4,R5,R6,R7}
+//EXITFUNC
+  POP {R1, R2, R3, R4, R5}
   BX LR
 
   END
--- es_user/src/func_khz.s
+++ es_user/src/func_khz.s
@@ -14,48 +14,38 @@ count_pulse_khz
   ; Funcao read_freq_hz
   ; Parametro de entrada: N�o tem
   ; Parametro de saida: R0 - Contagem de pulsos alternados
-  PUSH {R4,R5,R6,R7}
+  PUSH {R1, R2, R3, R4, R5}
   MOV R0, #0        ;toogle counter
-  MOV R1, #0        ;last state
   MOV R2, #0        ;read gpio
   MOV R3, #0        ;time acquire
-  LDR R4, =GPIO_PORTK_AHB_DATA_BITS_R				;Carrega o valor do offset do data register
+
+  LDR R4, =GPIO_PORTK_AHB_DATA_BITS_R	;Carrega o valor do offset do data register
   ADD R4, R4, #0x04	    ;Soma ao offset o endereço do bit0 para leitura amigavel [BIT0]
-  MOV R5, #0        ;time sampling
-  MOV R6, #0        ;for khz purpose
-  MOV R7, #0x3600   ; 240E6 
-  MOVT R7, #0x016E   ; são 240M ciclos de máquina, o que representa 1 segundo
+
+  //MOV  R5, #0x9680    ; Em 12 ciclos de máquina, 2.000.000 de leitura podem ser realizadas 
+  //MOVT R5, #0x0098    ; considerando um PLL de 24MHz
+  MOV  R5, #0x8480    ; Em 12 ciclos de máquina, 2.000.000 de leitura podem ser realizadas 
+  MOVT R5, #0x001E    ; considerando um PLL de 24MHz
+  MOV R3, #0        ;for khz purpose
+  LDR R1, [R4]      ;Last State
+
+
 NEWSAMPLING
   LDR R2, [R4]      ;Leitura GPIO
   CMP R2, R1
   ITT NE
-  ADDNE R6, #0x01   ;estado anterior diferente do atual, conta um pulso
+  ADDNE R3, #0x01   ;estado anterior diferente do atual, conta um pulso
   MOVNE R1, R2      ;salva estado anterior
   
-  CMP R6, #2000     ; 2 thousand cycles is equivalent to 1kHz
+  CMP R3, #1000     ; 1 thousand cycles is equivalent to 1kHz
   ITT GE
   ADDGE R0, R0, #1
-  MOVGE R6, #0
-
-  CMP R3, R7        ;verifica se já terminou a aquisição
-  BGE EXITFUNC
+  MOVGE R3, #0
   
-  NOP               ;Just to have even number of cycles
-  ADD R5, R5, #10
-WAITSAMPLING
-  NOP
-  CMP R5, #240     
-  ITTT GE
-  ADDGE R5, R5, #9
-  ADDGE R3, R3, R5
-  SUBGE R5, R5, #240
-  BGE NEWSAMPLING
-
-  ADD R5, R5, #10
-  B WAITSAMPLING
-
+  SUBS R5, #1     
+  BPL NEWSAMPLING   ;verifica se já terminou a aquisição
 EXITFUNC
-  POP {R4,R5,R6,R7}
+  POP {R1, R2, R3, R4, R5}
   BX LR
 
   END
